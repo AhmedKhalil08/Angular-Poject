cdc3c2890278d6a726aab5fc4b2f5fc78b66e6b7
Ahmed-Khail <ahmed.mohamed.hamdy08@gmail.com>
2026-05-02 08:05:36 +0300

    added filteration by name

--- ecommerce/src/Components/products/products.ts
+++ ecommerce/src/Components/products/products.ts
@@ -3,20 +3,27 @@ import productsData from '../../data/prd.json';
 import { IProduct } from '../../models/iproduct';
 import { DescripPipe } from '../../Pipes/descrip-pipe';
 import { Zoom } from '../../Directives/zoom';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-products',
-  imports: [DescripPipe,Zoom],
+  imports: [DescripPipe,Zoom,FormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
 export class Products {
-  showToast=false;
-  totalPrice:number;
- prdList : IProduct[];
- constructor(){
-  this.prdList=productsData;
-  this.totalPrice=0;
- }
+    showToast=false;
+    totalPrice:number;
+    categories:string[];  
+    prdList : IProduct[];
+    filteretedList:IProduct[];
+    selectedCategory: string = 'all';
+constructor(){
+this.prdList=productsData;
+this.totalPrice=0;
+this.categories = [...new Set(productsData.map(p => p.category))];
+this.filteretedList=productsData;    
+}
+
 buyProduct(Input: any, product: IProduct) {
     let Quan = Number(Input.value);
     if (Quan > product.stock) {
@@ -30,5 +37,16 @@ buyProduct(Input: any, product: IProduct) {
     this.totalPrice += Quan * product.price;
     product.stock -= Quan;
     Input.value = "";
-}  }
+} 
+filterByCategory(){
+    if(this.selectedCategory==="all"){
+        this.filteretedList=this.prdList;
+    }
+    else{
+        this.filteretedList=this.prdList.filter(p=>p.category===this.selectedCategory);
+    }
+}
+
+
+}
 
